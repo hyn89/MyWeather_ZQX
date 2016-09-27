900acdafaa28daff3bc152c47ee16baf6249889e
hyn <hyn89sg@gmail.com>
2016-09-27 18:44:05 +0800

    added class 5 code to update net

--- app/src/main/java/com/smart/hyn/myweather/MainActivity.java
+++ app/src/main/java/com/smart/hyn/myweather/MainActivity.java
@@ -1,13 +1,42 @@
 package com.smart.hyn.myweather;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.smart.hyn.myweather.util.NetUtil;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView mUpdateBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.weather_info);
+        setContentView(R.layout.weather_info;
+
+        mUpdateBtn = (ImageView)findViewById(R.id.title_update_btn);
+        mUpdateBtn.setOnClickListener(this);
+
+        if(NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE){
+            Log.d("myWeather", "Net OK");
+            Toast.makeText(MainActivity.this, "Net OK", Toast.LENGTH_LONG).show();
+        }else{
+            Log.d("myWeather","Net Fail");
+            Toast.makeText(MainActivity.this, "Net Fail", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.title_update_btn){
+            SharedPreferences sharedPreferences = getSharedPreferences("config", MODE_PRIVATE);
+            String cityCode = sharedPreferences.getString("main_city_code", "101010100");
+            Log.d("myWeather", cityCode);
+        }
     }
 }
